238cfb7df7b28fb4c45c57c46c2f3c33eb631dfd
Robert <ropak@wp.pl>
2017-03-16 19:20:16 +0100

    + Added test method for testing if result for two empty arrays if 1

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,8 +1,11 @@
 package edu.iis.mto.similarity;
 
 import org.junit.Before;
+import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.emptyIterable;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 public class SimilarityFinderTest {
 
@@ -13,5 +16,14 @@ public class SimilarityFinderTest {
     public void setUp() throws Exception {
         mockSequenceSeeker = new MockSequenceSeeker();
         finder = new SimilarityFinder(mockSequenceSeeker);
+    }
+
+    @Test
+    public void testCalculateJackardSimilarity_EmptyArrays() throws Exception {
+
+        double actual = finder.calculateJackardSimilarity(new int[0], new int[0]);
+
+        assertThat(actual, is(1.d));
+        assertThat(mockSequenceSeeker.calls, emptyIterable());
     }
 }
