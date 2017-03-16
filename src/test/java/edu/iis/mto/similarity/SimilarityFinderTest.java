768a6040783b56c2a536e066e6c73dd9080bbe34
Robert <ropak@wp.pl>
2017-03-16 20:26:12 +0100

    - Removed repetead code with new function: performTestAndAssert

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -26,6 +26,17 @@ public class SimilarityFinderTest {
         assertThat(mockSequenceSeeker.calls, emptyIterable());
     }
 
+    private void performTestAndAssert(double expected, int[] ints1, int[] ints2) {
+        double actual = finder.calculateJackardSimilarity(ints1, ints2);
+
+        assertThat(actual, is(expected));
+
+        for(MockSequenceSeeker.Call call : mockSequenceSeeker.calls) {
+            assertThat(call.param, isIn(intsToArray(ints1)));
+            assertThat(call.sequence, is(ints2));
+        }
+    }
+
     private static Integer[] intsToArray(int[] array) {
         Integer[] result = new Integer[array.length];
         for (int i = 0; i < array.length; i++) {
@@ -40,14 +51,7 @@ public class SimilarityFinderTest {
         int[] ints2 = new int[ints1.length];
         System.arraycopy(ints1, 0, ints2, 0, ints1.length);
 
-        double actual = finder.calculateJackardSimilarity(ints1, ints2);
-
-        assertThat(actual, is(1.0d));
-
-        for(MockSequenceSeeker.Call call : mockSequenceSeeker.calls) {
-            assertThat(call.param, isIn(intsToArray(ints1)));
-            assertThat(call.sequence, is(ints2));
-        }
+        performTestAndAssert(1, ints1, ints2);
 
     }
 
@@ -56,14 +60,7 @@ public class SimilarityFinderTest {
         int[] ints1 = new int[]{1, 5, -3, 12, 8};
         int[] ints2 = new int[]{0, -5, 9, 13, 169};
 
-        double actual = finder.calculateJackardSimilarity(ints1, ints2);
-
-        assertThat(actual, is(0.0d));
-
-        for(MockSequenceSeeker.Call call : mockSequenceSeeker.calls) {
-            assertThat(call.param, isIn(intsToArray(ints1)));
-            assertThat(call.sequence, is(ints2));
-        }
+        performTestAndAssert(0.0d, ints1, ints2);
     }
 
     @Test
@@ -71,14 +68,7 @@ public class SimilarityFinderTest {
         int[] ints1 = new int[]{1, 5, 10};
         int[] ints2 = new int[]{0, 1, 8};
 
-        double actual = finder.calculateJackardSimilarity(ints1, ints2);
-
-        assertThat(actual, is(0.2d));
-
-        for(MockSequenceSeeker.Call call : mockSequenceSeeker.calls) {
-            assertThat(call.param, isIn(intsToArray(ints1)));
-            assertThat(call.sequence, is(ints2));
-        }
+        performTestAndAssert(0.2, ints1, ints2);
     }
 
     @Test
@@ -86,13 +76,8 @@ public class SimilarityFinderTest {
         int[] ints1 = new int[]{2, 9, 13, 69};
         int[] ints2 = new int[]{13};
 
-        double actual = finder.calculateJackardSimilarity(ints1, ints2);
+        performTestAndAssert(0.25, ints1, ints2);
 
-        assertThat(actual, is(0.25d));
-
-        for(MockSequenceSeeker.Call call : mockSequenceSeeker.calls) {
-            assertThat(call.param, isIn(intsToArray(ints1)));
-            assertThat(call.sequence, is(ints2));
-        }
     }
+
 }
