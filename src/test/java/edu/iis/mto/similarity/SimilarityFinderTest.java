f932ee15380913997a87a74ff41facabbf779c56
Sasho <Sasho>
2017-03-17 22:32:10 +0100

    Dodano przypadek testowy z pustymi tablicami

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,5 +1,9 @@
 package edu.iis.mto.similarity;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 /**
@@ -7,4 +11,22 @@ import static org.junit.Assert.*;
  */
 public class SimilarityFinderTest {
 
+    private SimilarityFinder similarityFinder;
+    private Searcher searcher;
+
+    @Before
+    public void setUp() throws Exception {
+        searcher = new Searcher();
+        similarityFinder = new SimilarityFinder(searcher);
+    }
+
+    @Test
+    public void test_emptyArrays() throws Exception {
+        int[] tab1 = new int[0];
+        int[] tab2 = new int[0];
+
+        double actual = similarityFinder.calculateJackardSimilarity(tab1, tab2);
+
+        assertThat(actual, is(1.0d));
+    }
 }
