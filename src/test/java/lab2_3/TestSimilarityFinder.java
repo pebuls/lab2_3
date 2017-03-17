8e3972471891db07653afe088335b53080641eea
mateusz.sadowski <195054@edu.p.lodz.pl>
2017-03-17 18:16:06 +0100

    Added a test case for when arrays are null

--- src/test/java/lab2_3/TestSimilarityFinder.java
+++ src/test/java/lab2_3/TestSimilarityFinder.java
@@ -1,14 +1,21 @@
 package lab2_3;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
 
 import org.junit.Test;
 
+import edu.iis.mto.similarity.SearchForSequenceMocker;
+import edu.iis.mto.similarity.SimilarityFinder;
+
 public class TestSimilarityFinder {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@Test (expected = NullPointerException.class)
+	public void FindSimilarityNulls() {
+		SearchForSequenceMocker mocker = new SearchForSequenceMocker();
+		SimilarityFinder finder = new SimilarityFinder(mocker);
+		int[] seq1 = null, seq2 = null;
+		double result = finder.calculateJackardSimilarity(seq1, seq2);
 	}
 
 }
