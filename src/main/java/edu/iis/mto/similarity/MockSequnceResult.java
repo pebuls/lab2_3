507b2f4584a89507af1783d5eb8175b66ca033b3
Piotr Dudek <194936@edu.p.lodz.pl>
2017-03-18 16:44:03 +0100

    Poprawa nazwy klasy MockSequenceResult na MockSequenceSearcher i implementacja interfejsu SequenceSearcher

--- src/main/java/edu/iis/mto/similarity/MockSequnceResult.java
+++
@@ -1,7 +0,0 @@
-package edu.iis.mto.similarity;
-
-/**
- * Created by Piotrek on 18.03.2017.
- */
-public class MockSequnceResult {
-}
---
+++ src/main/java/edu/iis/mto/similarity/MockSequnceSearcher.java
@@ -0,0 +1,9 @@
+package edu.iis.mto.similarity;
+
+import edu.iis.mto.search.SequenceSearcher;
+
+/**
+ * Created by Piotrek on 18.03.2017.
+ */
+public class MockSequnceSearcher implements SequenceSearcher {
+}
