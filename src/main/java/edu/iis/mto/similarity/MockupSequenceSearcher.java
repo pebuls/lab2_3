47249a184d8140b2724da1b7142165e5ece6c008
Konrad Gos <goskonrad@outlook.com>
2017-03-18 21:56:29 +0100

    added implementation of SequenceSearch with default method for MockupSequenceSearcher

--- src/main/java/edu/iis/mto/similarity/MockupSequenceSearcher.java
+++ src/main/java/edu/iis/mto/similarity/MockupSequenceSearcher.java
@@ -1,7 +1,14 @@
 package edu.iis.mto.similarity;
 
+import edu.iis.mto.search.SearchResult;
+import edu.iis.mto.search.SequenceSearcher;
+
 /**
  * Created by Konrad Gos on 18.03.2017.
  */
-public class MockupSequenceSearcher {
+public class MockupSequenceSearcher implements SequenceSearcher {
+
+    public SearchResult search(int i, int[] ints) {
+        return null;
+    }
 }
