0949ad66b145a8a40932beac0caa18eccbe78ac5
Konrad Gos <goskonrad@outlook.com>
2017-03-18 22:09:44 +0100

    changed search method in MockupSequenceSearcher

--- src/main/java/edu/iis/mto/similarity/MockupSequenceSearcher.java
+++ src/main/java/edu/iis/mto/similarity/MockupSequenceSearcher.java
@@ -8,7 +8,12 @@ import edu.iis.mto.search.SequenceSearcher;
  */
 public class MockupSequenceSearcher implements SequenceSearcher {
 
-    public SearchResult search(int i, int[] ints) {
-        return null;
+    public SearchResult search(int index, int[] ints) {
+        for (int i=0; i<ints.length; i++) {
+            if(ints[i]==index) {
+                return new MockupSearchResult(true, index);
+            }
+        }
+        return new MockupSearchResult(false, -1);
     }
 }
