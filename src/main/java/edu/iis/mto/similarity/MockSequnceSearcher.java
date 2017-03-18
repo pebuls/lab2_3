5ab6476824dd8621b0555c33d901cc6615675fea
Piotr Dudek <194936@edu.p.lodz.pl>
2017-03-18 16:46:09 +0100

    Dodanie do klasy MockSequenceSearcher default'owej implementacji metody search

--- src/main/java/edu/iis/mto/similarity/MockSequnceSearcher.java
+++ src/main/java/edu/iis/mto/similarity/MockSequnceSearcher.java
@@ -1,9 +1,13 @@
 package edu.iis.mto.similarity;
 
+import edu.iis.mto.search.SearchResult;
 import edu.iis.mto.search.SequenceSearcher;
 
 /**
  * Created by Piotrek on 18.03.2017.
  */
 public class MockSequnceSearcher implements SequenceSearcher {
+    public SearchResult search(int i, int[] ints) {
+        return null;
+    }
 }
