0452c059f4964d2aa16ff1608fa91ded37227102
Piotr Dudek <194936@edu.p.lodz.pl>
2017-03-18 18:12:20 +0100

    Dodanie testu dla przypadku dwóch różnej długości sekwencji wejściowych o różnych argumentach

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -93,4 +93,13 @@ public class SimilarityFinderTest {
         double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
         Assert.assertThat(result, is(equalTo(0.5)));
     }
+
+    @Test
+    public void differentArraysNotSameLegth(){
+        int[] seq1 = {10, 20, 30};
+        int[] seq2 = {50, 15, 60, 65};
+
+        double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
+        Assert.assertThat(result, is(equalTo(0.0)));
+    }
 }
