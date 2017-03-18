da121a77e53cb06921fd7b7fcbb38ee015a28cf6
Piotr Dudek <194936@edu.p.lodz.pl>
2017-03-18 16:37:49 +0100

    Dodanie pól typu prywatnego do klasy MockSearchResult

--- src/main/java/edu/iis/mto/similarity/MockSearchResult.java
+++ src/main/java/edu/iis/mto/similarity/MockSearchResult.java
@@ -6,6 +6,10 @@ import edu.iis.mto.search.SearchResult;
  * Created by Piotrek on 18.03.2017.
  */
 public class MockSearchResult implements SearchResult{
+
+    private boolean found;
+    private int position;
+
     public boolean isFound() {
         return false;
     }
