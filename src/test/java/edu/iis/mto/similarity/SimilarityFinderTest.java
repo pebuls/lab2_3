30955d9ad8141ce71187975fe778f424d8fdb0db
Piotr Dudek <194936@edu.p.lodz.pl>
2017-03-18 17:38:56 +0100

    Dodanie testu dla przypadku identycznych sekwencji wejściowych

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,5 +1,10 @@
 package edu.iis.mto.similarity;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 /**
@@ -10,4 +15,12 @@ public class SimilarityFinderTest {
     private MockSequnceSearcher mockSequnceSearcher = new MockSequnceSearcher();
     private SimilarityFinder similarityFinder = new SimilarityFinder(mockSequnceSearcher);
 
+    @Test
+    public void sameArraysTest(){
+        int[] seq1 = {10, 20, 30};
+        int[] seq2 = {10, 20, 30};
+
+        double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
+        Assert.assertThat(result, is(equalTo(1.0)));
+    }
 }
