465ed32efe16a7467e13e7f602ae3f33dee65752
Konrad Gos <goskonrad@outlook.com>
2017-03-18 21:48:44 +0100

    added implements SearchResult and default methods for MockupSearchResult

--- src/main/java/edu/iis/mto/similarity/MockupSearchResult.java
+++ src/main/java/edu/iis/mto/similarity/MockupSearchResult.java
@@ -1,7 +1,17 @@
 package edu.iis.mto.similarity;
 
+import edu.iis.mto.search.SearchResult;
+
 /**
  * Created by Konrad Gos on 18.03.2017.
  */
-public class MockupSearchResult {
+public class MockupSearchResult implements SearchResult {
+
+    public boolean isFound() {
+        return false;
+    }
+
+    public int getPosition() {
+        return 0;
+    }
 }
