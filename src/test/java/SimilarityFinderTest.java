14b3d5e580c8dd60b38bc483f8d5f137fda6212c
Piotr Markowski <piotrjakubmarkowski@gmail.com>
2017-03-18 01:20:38 +0100

    test dla takich samych tablic

--- src/test/java/SimilarityFinderTest.java
+++ src/test/java/SimilarityFinderTest.java
@@ -1,5 +1,10 @@
 import edu.iis.mto.similarity.MockSequenceSearcher;
 import edu.iis.mto.similarity.SimilarityFinder;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * Created by Piotr on 18.03.2017.
@@ -9,4 +14,14 @@ public class SimilarityFinderTest {
     private MockSequenceSearcher mockSequenceSearcher = new MockSequenceSearcher();
     private SimilarityFinder similarityFinder = new SimilarityFinder(mockSequenceSearcher);
 
+    @Test
+    public void sameSimilarity() {
+        final double EXPECTED_RESULT = 1.0;
+        final int[] ARR1 = {1, 2, 3, 4};
+        final int[] ARR2 = {1, 2, 3, 4};
+
+        double result = similarityFinder.calculateJackardSimilarity(ARR1, ARR2);
+
+        assertThat(result, is(EXPECTED_RESULT));
+    }
 }
