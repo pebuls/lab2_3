252e4d8d359dc921596faf5d46fc0fcd436471cf
Piotr Dudek <194936@edu.p.lodz.pl>
2017-03-18 18:18:26 +0100

    Poprawienie klasy MockSequnceSearcher, aby liczyła ilość wywołań

--- src/main/java/edu/iis/mto/similarity/MockSequnceSearcher.java
+++ src/main/java/edu/iis/mto/similarity/MockSequnceSearcher.java
@@ -7,7 +7,16 @@ import edu.iis.mto.search.SequenceSearcher;
  * Created by Piotrek on 18.03.2017.
  */
 public class MockSequnceSearcher implements SequenceSearcher {
+
+    public int callsCounter = 0;
+    public int key = -1;
+    public int seq[] = null;
+
     public SearchResult search(int key, int[] seq) {
+
+        this.key = key;
+        this.seq = seq;
+        callsCounter++;
 
         for(int i = 0; i < seq.length; i++){
             if(seq[i] == key){
