837545f4705c84407713471f7645c40c2f773ec4
Andrzej Borzęcki <andrew.borzecki@gmail.com>
2017-03-19 03:11:23 +0100

    Added setUp method for SimilarityFinder test class

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,8 +1,19 @@
 package edu.iis.mto.similarity;
 
+import org.junit.Before;
+
 /**
  * Created by grusz on 19.03.2017.
  */
 public class SimilarityFinderTest {
 
+    private MockSequenceSeeker mockSequenceSeeker;
+    private SimilarityFinder finder;
+
+    @Before public void setUp() throws Exception {
+        mockSequenceSeeker = new MockSequenceSeeker();
+        finder = new SimilarityFinder(mockSequenceSeeker);
+    }
+
+    
 }
