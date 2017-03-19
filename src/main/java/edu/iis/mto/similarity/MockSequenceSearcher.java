c0a597a9fded79c23c694578fc0138bc7f14e831
patrykxk <patrix215@gmail.com>
2017-03-19 15:33:42 +0100

    Uzupelnienie metody search w SequenceSearcher

--- src/main/java/edu/iis/mto/similarity/MockSequenceSearcher.java
+++ src/main/java/edu/iis/mto/similarity/MockSequenceSearcher.java
@@ -3,11 +3,19 @@ package edu.iis.mto.similarity;
 import edu.iis.mto.search.SearchResult;
 import edu.iis.mto.search.SequenceSearcher;
 
-/**
- * Created by Patryk on 2017-03-19.
- */
+
 public class MockSequenceSearcher implements SequenceSearcher {
-    public SearchResult search(int i, int[] ints) {
-        return null;
+    public SearchResult search(int key, int[] ints) {
+        if(ints.length==0){
+            throw new IllegalArgumentException();
+        }
+        
+        for(int i=0;i<ints.length;i++){
+            if(ints[i]==key){
+                return new MockSearchResult(true, i);
+            }
+        }
+
+        return new MockSearchResult(false, -1);
     }
 }
