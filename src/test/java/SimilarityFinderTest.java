a266dd26261c393aaaa66c1e3c50e2cd9af54345
patrykxk <patrix215@gmail.com>
2017-03-19 16:10:07 +0100

    Test pustych sekwencji

--- src/test/java/SimilarityFinderTest.java
+++ src/test/java/SimilarityFinderTest.java
@@ -7,12 +7,21 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
 public class SimilarityFinderTest {
+    private SimilarityFinder similarityFinder = new SimilarityFinder(new MockSequenceSearcher());
     @Test
     public void sameSequencesTest(){
-        SimilarityFinder similarityFinder = new SimilarityFinder(new MockSequenceSearcher());
         int seq1[] = {1,2,3};
         int seq2[] = {1,2,3};
         double actual = similarityFinder.calculateJackardSimilarity(seq1,seq2);
         Assert.assertThat(actual, is(equalTo(1.0)));
     }
+    @Test
+    public void emptySequencesTest(){
+        int seq1[] = {};
+        int seq2[] = {};
+        double actual = similarityFinder.calculateJackardSimilarity(seq1,seq2);
+        System.out.println(actual);
+        Assert.assertThat(actual, is(equalTo(1.0)));
+    }
+
 }
