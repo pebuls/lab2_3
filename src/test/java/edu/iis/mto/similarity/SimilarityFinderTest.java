c4b5e706ec93433645c0d542ef0202e7412dbb67
Konrad Gos <goskonrad@outlook.com>
2017-03-19 02:26:34 +0100

    added test to check two empty sequences for method in SimilarityFinderTest

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,10 +1,26 @@
 package edu.iis.mto.similarity;
 
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 /**
  * Created by Konrad Gos on 18.03.2017.
  */
 public class SimilarityFinderTest {
+
+    @Test
+    public void checkTwoEmptySequence() {
+        MockupSequenceSearcher mockupSequenceSearcher = new MockupSequenceSearcher();
+        SimilarityFinder similarityFinder = new SimilarityFinder(mockupSequenceSearcher);
+
+        int[] firstSeqeunce = new int[0];
+        int[] secondSeqeunce = new int[0];
+
+        double similarityResult = similarityFinder.calculateJackardSimilarity(firstSeqeunce,secondSeqeunce);
+
+        assertThat(similarityResult, is(1.0));
+    }
 
 }
