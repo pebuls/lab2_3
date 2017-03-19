a06149c847f1e6d58a2b7019aa403250ebc09a6d
patrykxk <patrix215@gmail.com>
2017-03-19 15:24:24 +0100

    Stworzenie klasy z implementacją interfejsu SequenceSearcher

---
+++ src/main/java/edu/iis/mto/similarity/MockSequenceSearcher.java
@@ -0,0 +1,13 @@
+package edu.iis.mto.similarity;
+
+import edu.iis.mto.search.SearchResult;
+import edu.iis.mto.search.SequenceSearcher;
+
+/**
+ * Created by Patryk on 2017-03-19.
+ */
+public class MockSequenceSearcher implements SequenceSearcher {
+    public SearchResult search(int i, int[] ints) {
+        return null;
+    }
+}
