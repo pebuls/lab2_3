a2c7e305758bad16cd86bdc6425ba26402805afe
Andrzej Borzęcki <andrew.borzecki@gmail.com>
2017-03-19 03:07:31 +0100

    Added calls counting to MockSequenceSeeker, fixed search method

--- src/test/java/edu/iis/mto/similarity/MockSequenceSeeker.java
+++ src/test/java/edu/iis/mto/similarity/MockSequenceSeeker.java
@@ -3,10 +3,14 @@ package edu.iis.mto.similarity;
 import edu.iis.mto.search.SearchResult;
 import edu.iis.mto.search.SequenceSearcher;
 
+import java.util.ArrayList;
+
 /**
  * Created by grusz on 19.03.2017.
  */
 public class MockSequenceSeeker implements SequenceSearcher {
+
+    ArrayList<SearchMethodCall> calls = new ArrayList<SearchMethodCall>();
 
     private class MockSearchResult implements SearchResult {
 
@@ -27,7 +31,26 @@ public class MockSequenceSeeker implements SequenceSearcher {
         }
     }
 
+    public static class SearchMethodCall {
+        private int searchedKey;
+        private int[] searchedSequence;
+
+        public SearchMethodCall(int searchedKey, int[] searchedSequence) {
+            this.searchedKey = searchedKey;
+            this.searchedSequence = searchedSequence;
+        }
+
+        public int getSearchedKey() {
+            return searchedKey;
+        }
+
+        public int[] getSearchedSequence() {
+            return searchedSequence;
+        }
+    }
+
     public SearchResult search(int i, int[] ints) {
+        calls.add(i,ints);
         for(int j=0;j<ints.length;j++){
             if(ints[j]==i){
                 return new MockSearchResult(true,j);
