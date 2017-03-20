b2f61ba948f382eb65520b98ee2b4202cc638521
Szczepaniak.Wojciech <195067@edu.p.lodz.pl>
2017-03-21 00:49:03 +0100

    Add simple search() method calls counting.

--- src/test/java/mockClasses/MockSequenceSearcher.java
+++ src/test/java/mockClasses/MockSequenceSearcher.java
@@ -8,7 +8,17 @@ import edu.iis.mto.search.SequenceSearcher;
  * Created by Wojciech Szczepaniak on 20.03.2017.
  */
 public class MockSequenceSearcher implements SequenceSearcher {
+
+    private int call = 0;
+
+    public int getCall() {
+        int returned = call;
+        call = 0;
+        return returned;
+    }
+
     public SearchResult search(int i, int[] ints) {
+        ++call;
         return new MockSearchResult(Ints.asList(ints).indexOf(i));
     }
 }
