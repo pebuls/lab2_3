6a66209db282fc77f7ea3dddd702a47d983b62ca
Szyrner.Milosz <195069@edu.p.lodz.pl>
2017-03-20 21:26:13 +0100

    Utworzenie klasy testowej dla klasy SimilarityFinder

---
+++ src/test/java/lab2_3/SimilarityFinderTest.java
@@ -0,0 +1,14 @@
+package lab2_3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimilarityFinderTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
