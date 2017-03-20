31ac7d822319cb57d1e257638babe63ec011a9d1
Szczepaniak.Wojciech <195067@edu.p.lodz.pl>
2017-03-20 23:39:00 +0100

    Create SimilarityFinder test: calculateJackardSimilarity() method returns zero when one-element sequences are different.

--- src/test/java/SimilarityFinderTest.java
+++ src/test/java/SimilarityFinderTest.java
@@ -46,4 +46,14 @@ public class SimilarityFinderTest {
         Assert.assertThat(finder.calculateJackardSimilarity(seq1, seq2), is(EXPECTED));
         Assert.assertThat(finder.calculateJackardSimilarity(seq2, seq1), is(EXPECTED));
     }
+
+    @Test
+    public void returnZeroWhenOneElementSequencesAreDifferent() {
+        int[] seq1 = {1};
+        int[] seq2 = {2};
+        final double EXPECTED = 0.0;
+
+        Assert.assertThat(finder.calculateJackardSimilarity(seq1, seq2), is(EXPECTED));
+        Assert.assertThat(finder.calculateJackardSimilarity(seq2, seq1), is(EXPECTED));
+    }
 }
