98b0e437e4683d951778238df46b315be0998c2a
Szyrner.Milosz <195069@edu.p.lodz.pl>
2017-03-20 22:16:12 +0100

    Dodanie licznika wywołań w implementacji interfejsu SequenceSearcher

--- src/main/java/edu/iis/mto/similarity/MockSequenceSearcher.java
+++ src/main/java/edu/iis/mto/similarity/MockSequenceSearcher.java
@@ -4,7 +4,10 @@ import edu.iis.mto.search.SearchResult;
 import edu.iis.mto.search.SequenceSearcher;
 
 public class MockSequenceSearcher implements SequenceSearcher{
+	
+	public int callCounter = 0;
 	public SearchResult search(int key, int[] seq) {
+		callCounter++;
 		for(int i = 0; i < seq.length; i++) {
 			if(seq[i] == key){
 				return new MockSearchResult(true, i);
