95066b921edf46bd7e516d4d7decbe7b4eb19c1d
Szczepaniak.Wojciech <195067@edu.p.lodz.pl>
2017-03-20 22:55:17 +0100

    Create SimilarityFinder test: calculateJackardSimilarity() method returns one when both sequences are empty.

--- src/test/java/SimilarityFinderTest.java
+++ src/test/java/SimilarityFinderTest.java
@@ -1,7 +1,10 @@
 import edu.iis.mto.similarity.SimilarityFinder;
 import mockClasses.MockSequenceSearcher;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.*;
 
 /**
  * Created by Wojciech Szczepaniak on 20.03.2017.
@@ -13,5 +16,14 @@ public class SimilarityFinderTest {
     @Before
     public void init() {
         finder = new SimilarityFinder(new MockSequenceSearcher());
+    }
+
+    @Test
+    public void returnOneWhenSequencesAreEmpty() {
+        int[] seq1 = {};
+        int[] seq2 = {};
+        final double EXPECTED = 1.0;
+
+        Assert.assertThat(finder.calculateJackardSimilarity(seq1, seq2), is(EXPECTED));
     }
 }
