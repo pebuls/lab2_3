0d416d82ae386a93f73f02226af9e0e934b7566f
Klaudia Głuszek <klaudia.gluszek@gmail.com>
2017-03-20 12:16:02 +0100

    dodanie testu sprawdzającego czy metoda w search została wywoałana określoną liczbę razy

--- src/main/java/edu/iis/mto/similarity/MockSequenceSearcher.java
+++ src/main/java/edu/iis/mto/similarity/MockSequenceSearcher.java
@@ -7,15 +7,21 @@ import edu.iis.mto.search.SequenceSearcher;
  * Created by Klaudia on 20.03.2017.
  */
 public class MockSequenceSearcher implements SequenceSearcher {
+    private int callCounter = 0;
 
     public SearchResult search(int i, int[] ints) {
         int pos = 0;
+        callCounter++;
         for (int j : ints) {
             if (i == j) {
-                return new MockSearchResult(true,pos);
+                return new MockSearchResult(true, pos);
             }
             pos++;
         }
         return new MockSearchResult(false, -1);
+    }
+
+    public int getCallCounter() {
+        return callCounter;
     }
 }
--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,7 +1,9 @@
 package edu.iis.mto.similarity;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.comparesEqualTo;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
@@ -9,33 +11,77 @@ import static org.junit.Assert.*;
  * Created by Klaudia on 20.03.2017.
  */
 public class SimilarityFinderTest {
+    SimilarityFinder similarityFinder;
+    private MockSequenceSearcher dummySequenceSearcher;
+
+    @Before
+    public void setUp() {
+        dummySequenceSearcher = createDummySequenceSearcher();
+        similarityFinder = new SimilarityFinder(dummySequenceSearcher);
+
+    }
+
+    private MockSequenceSearcher createDummySequenceSearcher() {
+        return new MockSequenceSearcher();
+    }
+
     @Test
     public void testCalculateJackardSimilarityAnotherSizeSeq() throws Exception {
-        SimilarityFinder similarityFinder = new SimilarityFinder(new MockSequenceSearcher());
-        int[] seq1 = new int[] {1,2,3,4,5};
-        int[] seq2 = new int[] {1,2,3};
-        double expected = (double) 3/5;
+        int[] seq1 = new int[]{1, 2, 3, 4, 5};
+        int[] seq2 = new int[]{1, 2, 3};
+        double expected = (double) 3 / 5;
 
-        double result = similarityFinder.calculateJackardSimilarity(seq1,seq2);
+        double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
 
         assertThat(result, is(expected));
     }
 
     @Test
-    public void testcalculateJackardEmptySeq() throws Exception {
-        SimilarityFinder similarityFinder = new SimilarityFinder(new MockSequenceSearcher());
-        int[] seq1 = new int[] {};
-        int[] seq2 = new int[] {};
+    public void testCalculateJackardEmptySeq() throws Exception {
+        int[] seq1 = new int[]{};
+        int[] seq2 = new int[]{};
         double expected = (double) 1;
 
-        double result = similarityFinder.calculateJackardSimilarity(seq1,seq2);
+        double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
 
         assertThat(result, is(expected));
     }
 
+    @Test
+    public void testCalculateJackardTheSameContentSeq() {
+        int[] seq = new int[]{1, 2, 3};
 
+        double expected = (double) 1;
 
+        double result = similarityFinder.calculateJackardSimilarity(seq, seq);
 
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void testCalculateJackardTheSameSeqLenAnotherContents() {
+        int[] seq = new int[]{1, 2, 3};
+        int[] seq1 = new int[]{1, 2, 5};
+
+        double expected = (double) 2 / 4;
+
+        double result = similarityFinder.calculateJackardSimilarity(seq1, seq);
+
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void testCalculateJackardSearchCallNumber() {
+        int[] seq = new int[]{1, 2, 3};
+        int[] seq1 = new int[]{1, 2, 5, 4};
+
+        int expected = 4;
+
+        double result = similarityFinder.calculateJackardSimilarity(seq1, seq);
+
+        assertThat(dummySequenceSearcher.getCallCounter(), is(expected));
+
+    }
 
 
 }
