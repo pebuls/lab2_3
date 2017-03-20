30e959259d9229d1481dc3ed52235aafec2b6d36
Robert Jankowski <jankowski.r@hotmail.com>
2017-03-21 00:53:50 +0100

    half and quarter similarity test

--- src/test/java/tests/TestSimilarityFinder.java
+++ src/test/java/tests/TestSimilarityFinder.java
@@ -1,6 +1,5 @@
 package tests;
 
-
 import edu.iis.mto.search.SequenceSearcher;
 import edu.iis.mto.similarity.DummySequenceSearcher;
 import org.junit.Test;
@@ -8,9 +7,8 @@ import edu.iis.mto.similarity.SimilarityFinder;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * To change this license header, choose License Headers in Project Properties. To change this template file, choose
+ * Tools | Templates and open the template in the editor.
  */
 
 /**
@@ -18,23 +16,44 @@ import static org.junit.Assert.assertThat;
  * @author jankowskirobert
  */
 public class TestSimilarityFinder {
-    
+
     @Test
-    public void testCalculateJackardSimilarityEmptySqeuence(){
+    public void testCalculateJackardSimilarityEmptySqeuence() {
         SequenceSearcher searcher = new DummySequenceSearcher();
-        SimilarityFinder finder = new SimilarityFinder(searcher);        
+        SimilarityFinder finder = new SimilarityFinder(searcher);
         final int[] emptySeq = new int[] {};
         double result = finder.calculateJackardSimilarity(emptySeq, emptySeq);
         assertThat(result, is(1.0d));
     }
-    
+
     @Test
-    public void testCalculateJackardSimilaritySameSequenceSize(){
+    public void testCalculateJackardSimilaritySameSequenceSize() {
         SequenceSearcher searcher = new DummySequenceSearcher();
-        SimilarityFinder finder = new SimilarityFinder(searcher);        
-        final int[] seq1 = new int[] {1,2,3,4};
-        final int[] seq2 = new int[] {1,2,3,4};
+        SimilarityFinder finder = new SimilarityFinder(searcher);
+        final int[] seq1 = new int[] {1, 2, 3, 4};
+        final int[] seq2 = new int[] {1, 2, 3, 4};
         double result = finder.calculateJackardSimilarity(seq1, seq2);
         assertThat(result, is(1.0));
     }
+
+    @Test
+    public void testCalculateJackardSimilarityHalf() {
+        SequenceSearcher searcher = new DummySequenceSearcher();
+        SimilarityFinder finder = new SimilarityFinder(searcher);
+        final int[] seq1 = new int[] {1, 2, 3, 4};
+        final int[] seq2 = new int[] {1, 2};
+        double result = finder.calculateJackardSimilarity(seq1, seq2);
+        assertThat(result, is(0.5));
+    }
+
+    @Test
+    public void testCalculateJackardSimilarityQuarter() {
+        SequenceSearcher searcher = new DummySequenceSearcher();
+        SimilarityFinder finder = new SimilarityFinder(searcher);
+        final int[] seq1 = new int[] {1, 2, 3, 4};
+        final int[] seq2 = new int[] {1};
+        double result = finder.calculateJackardSimilarity(seq1, seq2);
+        assertThat(result, is(0.25));
+    }
+
 }
