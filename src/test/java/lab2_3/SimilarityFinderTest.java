0067f47c6806ad0bb2629c75e91d065e8e229832
Szyrner.Milosz <195069@edu.p.lodz.pl>
2017-03-20 21:36:15 +0100

    Dodanie testu w którym obie tablice są null'ami

--- src/test/java/lab2_3/SimilarityFinderTest.java
+++ src/test/java/lab2_3/SimilarityFinderTest.java
@@ -2,13 +2,29 @@ package lab2_3;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
-public class SimilarityFinderTest {
+import edu.iis.mto.similarity.MockSequenceSearcher;
+import edu.iis.mto.similarity.SimilarityFinder;
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+public class SimilarityFinderTest {
+	
+	private SimilarityFinder finder;
+	private MockSequenceSearcher searcher;
+	
+	@Before
+	public void initialize() {
+		searcher = new MockSequenceSearcher();
+		finder = new SimilarityFinder(searcher);
+	}
+
+	@Test (expected = NullPointerException.class)
+	public void testThenTwoArraysAreNull() {
+		int seq1[] = null;
+		int seq2[] = null;
+		
+		finder.calculateJackardSimilarity(seq1, seq2);
 	}
 
 }
