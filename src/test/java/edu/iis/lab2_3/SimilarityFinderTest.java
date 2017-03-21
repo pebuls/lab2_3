15ab593b830edc14f2dfbd178c81d7767fd5e43b
Jakub Olczak <kuba.olczak.jo@gmail.com>
2017-03-21 14:48:12 +0100

    Dodanie 1 testu do SimilarityFinderTest i uzupełnienie importów

--- src/test/java/edu/iis/lab2_3/SimilarityFinderTest.java
+++ src/test/java/edu/iis/lab2_3/SimilarityFinderTest.java
@@ -1,14 +1,24 @@
 package edu.iis.lab2_3;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
 
 import org.junit.Test;
 
-public class SimilarityFinderTest {
+import edu.iis.mto.similarity.SearchForSequenceMocker;
+import edu.iis.mto.similarity.SimilarityFinder;
 
+public class SimilarityFinderTest {
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testFindSimilarityEmptyArrays() {
+		
+		SearchForSequenceMocker mocker = new SearchForSequenceMocker();
+		SimilarityFinder finder = new SimilarityFinder(mocker);
+		int[] seq1 = new int[0];
+		int[] seq2 = new int[0];
+		double result = finder.calculateJackardSimilarity(seq1, seq2);
+		assertThat(result, is(equalTo(1.0)));
 	}
 
 }
