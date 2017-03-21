5078f10bf9552a6a4e450ad24729d339069fe1f7
Aleksander Kaczmarczyk <axelkko@gmail.com>
2017-03-21 10:57:02 +0100

    MockSequenceSearcher: implementacja metod

--- src/test/java/edu/iis/mto/similarity/MockSequenceSearcher.java
+++ src/test/java/edu/iis/mto/similarity/MockSequenceSearcher.java
@@ -4,10 +4,23 @@ import edu.iis.mto.search.SearchResult;
 import edu.iis.mto.search.SequenceSearcher;
 
 public class MockSequenceSearcher implements SequenceSearcher {
-
+	
+	private int invocationsNumber = 0;
+	
 	public SearchResult search(int key, int[] seq) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		this.invocationsNumber++;
+		
+		for (int i=0; i<seq.length; i++) {
+			if (key == seq[i]) {
+				return new MockSearchResult(true, i);
+			}
+		}
+		
+		return new MockSearchResult(false, -1);
 	}
-
+	
+	public int getInvocationsNumber() {
+		return this.invocationsNumber;
+	}
 }
