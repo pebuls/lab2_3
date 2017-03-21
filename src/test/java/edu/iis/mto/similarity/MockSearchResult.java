9f0d6fdf13ec58b5813f2c07ec6b68399765b2db
Aleksander Kaczmarczyk <axelkko@gmail.com>
2017-03-21 10:43:05 +0100

    Utworzenie dublera: MockSearchResult

---
+++ src/test/java/edu/iis/mto/similarity/MockSearchResult.java
@@ -0,0 +1,17 @@
+package edu.iis.mto.similarity;
+
+import edu.iis.mto.search.SearchResult;
+
+public class MockSearchResult implements SearchResult {
+
+	public boolean isFound() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public int getPosition() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
