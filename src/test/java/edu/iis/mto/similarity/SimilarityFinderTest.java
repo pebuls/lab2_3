c08d0ff202b6bc78dd3ed410704e82e67e72f76d
Rafal Franiewski <194944@edu.p.lodz.pl>
2017-03-21 14:34:49 +0100

    Added test case for empty seq & added case for sameSeq

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,6 +1,11 @@
 package edu.iis.mto.similarity;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.isIn;
+import static org.hamcrest.Matchers.isOneOf;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created by RFran on 21.03.2017.
@@ -10,10 +15,32 @@ public class SimilarityFinderTest {
     private MockSequenceSeeker mockSequenceSeeker;
     private SimilarityFinder finder;
 
+    private Integer[] convertIntArrayToIntegerArray(int[] intArray){
+        Integer[] objectArray = new Integer[intArray.length];
+
+        for(int ctr = 0; ctr < intArray.length; ctr++) {
+            objectArray[ctr] = intArray[ctr];
+        }
+        return objectArray;
+    }
+
     @Before public void setUp() throws Exception {
         mockSequenceSeeker = new MockSequenceSeeker();
         finder = new SimilarityFinder(mockSequenceSeeker);
     }
 
+    @Test public void calculateJackardSimilarity_sameSequences(){
+        int[] seq1 = {1,2,3,4,5};
+        int[] seq2 = seq1;
+        double result = finder.calculateJackardSimilarity(seq1,seq2);
 
+        Integer[] convertedSeq1 = convertIntArrayToIntegerArray(seq1);
+
+        for(MockSequenceSeeker.SearchMethodCall call : mockSequenceSeeker.calls){
+
+            assertThat(call.getSearchedKey(),isIn(convertedSeq1));
+            assertThat(call.getSearchedSequence(),isOneOf(seq1,seq2));
+
+        }
+    }
 }
