393317fbe0948d807a3d5e4ef883d2cd66890259
Jakub Stawiszyński <kubastyk@wp.pl>
2017-03-21 12:22:50 +0100

    add test [ two different arrays ]

---
+++ src/test/java/lab2_3/SimilarityFinderTest.java
@@ -0,0 +1,28 @@
+package lab2_3;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+import edu.iis.mto.search.SequenceSearcher;
+import edu.iis.mto.similarity.SequenceSearcherImpl;
+import edu.iis.mto.similarity.SimilarityFinder;
+
+public class SimilarityFinderTest {
+
+	SequenceSearcher sequenceSearcher = new SequenceSearcherImpl();
+	SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSearcher);
+
+	@Test
+	public void differentArraysTest() {
+
+		int[] seq1 = {1,2,3};
+		int[] seq2 = {10,20,30};
+
+		double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
+
+		assertThat(result, is(equalTo(0.0)));
+	}
+
+}
