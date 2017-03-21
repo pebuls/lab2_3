5ea197ce76fb5ff34c05262dca567cea2bb2c321
Jakub Stawiszyński <kubastyk@wp.pl>
2017-03-21 12:05:13 +0100

    add implementation of SequenceSearcher interface

---
+++ src/main/java/edu/iis/mto/similarity/SequenceSearcherImpl.java
@@ -0,0 +1,18 @@
+package edu.iis.mto.similarity;
+
+import edu.iis.mto.search.SearchResult;
+import edu.iis.mto.search.SequenceSearcher;
+
+public class SequenceSearcherImpl implements SequenceSearcher {
+
+	public SearchResult search(int key, int[] seq) {
+		for (int j = 0; j < seq.length; j++) {
+			if (seq[j] == key) {
+				return new SearchResultImpl(true, j);
+			}
+		}
+		return new SearchResultImpl(false, -1);
+
+	}
+
+}
