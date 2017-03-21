798264ce9b657f3ded21ff27749b283309e3d711
Aleksander Kaczmarczyk <axelkko@gmail.com>
2017-03-21 09:55:07 +0100

    Utworzenie klasy testowej SimilarityFinderTest

---
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -0,0 +1,19 @@
+package edu.iis.mto.similarity;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SimilarityFinderTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
