e29d268326e923b0d194205e6b58e0bdaf5b7960
Rafal Franiewski <194944@edu.p.lodz.pl>
2017-03-21 14:58:51 +0100

    Added new test for sameSeq

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -3,8 +3,10 @@ package edu.iis.mto.similarity;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.emptyIterable;
 import static org.hamcrest.Matchers.isIn;
 import static org.hamcrest.Matchers.isOneOf;
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
@@ -33,7 +35,32 @@ public class SimilarityFinderTest {
         int[] seq1 = {1,2,3,4,5};
         int[] seq2 = seq1;
         double result = finder.calculateJackardSimilarity(seq1,seq2);
+        assertThat(result,is(1.0));
 
+        Integer[] convertedSeq1 = convertIntArrayToIntegerArray(seq1);
+
+        for(MockSequenceSeeker.SearchMethodCall call : mockSequenceSeeker.calls){
+
+            assertThat(call.getSearchedKey(),isIn(convertedSeq1));
+            assertThat(call.getSearchedSequence(),isOneOf(seq1,seq2));
+
+        }
+    }
+
+    @Test public void calculateJackardSimilarity_emptySequences(){
+        int[] seq1 = {};
+        int[] seq2 = seq1;
+        double result = finder.calculateJackardSimilarity(seq1,seq2);
+        assertThat(result,is(1.0));
+        assertThat(mockSequenceSeeker.calls, emptyIterable());
+
+    }
+
+    @Test public void calculateJackardSimilarity_PartiallySameSequences(){
+        int[] seq1 = {1,2,3,4};
+        int[] seq2 = {2,3,4,5};
+        double result = finder.calculateJackardSimilarity(seq1,seq2);
+        assertThat(result,is(0.6));
         Integer[] convertedSeq1 = convertIntArrayToIntegerArray(seq1);
 
         for(MockSequenceSeeker.SearchMethodCall call : mockSequenceSeeker.calls){
