6c388a07d30523c64c9b510cf8738b99c62efa7f
Aleksander Kaczmarczyk <axelkko@gmail.com>
2017-03-21 11:08:56 +0100

    SimilarityFinderTest: impl testu dla identycznych sekwencji

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -32,7 +32,12 @@ public class SimilarityFinderTest {
 	
 	@Test
 	public void testCalculateJackardSimilarityOfEqualSequences() {
-		fail("Not yet implemented");
+		
+		int[] seq1 = {1, 3, 5};
+		int[] seq2 = {1, 3, 5};
+		double expectedResult = 1.0d;
+		
+		Assert.assertThat(finder.calculateJackardSimilarity(seq1, seq2), is(equalTo(expectedResult)));
 	}
 	
 	@Test
