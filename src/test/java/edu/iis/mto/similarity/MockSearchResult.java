f04850cdff2ec76a897f57e1c833c29b65654573
Aleksander Kaczmarczyk <axelkko@gmail.com>
2017-03-21 10:45:14 +0100

    MockSearchResult: dodanie pol i uzupelnienie dziedziczonych metod

--- src/test/java/edu/iis/mto/similarity/MockSearchResult.java
+++ src/test/java/edu/iis/mto/similarity/MockSearchResult.java
@@ -3,15 +3,16 @@ package edu.iis.mto.similarity;
 import edu.iis.mto.search.SearchResult;
 
 public class MockSearchResult implements SearchResult {
-
+	
+	private boolean found;
+	private int position;
+	
 	public boolean isFound() {
-		// TODO Auto-generated method stub
-		return false;
+		return found;
 	}
 
 	public int getPosition() {
-		// TODO Auto-generated method stub
-		return 0;
+		return position;
 	}
 
 }
