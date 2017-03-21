b49d062912f2a0dd9ad8a10aa0499d8f8c8be9d0
Jakub Olczak <kuba.olczak.jo@gmail.com>
2017-03-21 14:57:46 +0100

    Dodanie 6 testu do klasy SimilarityFinderTest

--- src/test/java/edu/iis/lab2_3/SimilarityFinderTest.java
+++ src/test/java/edu/iis/lab2_3/SimilarityFinderTest.java
@@ -65,5 +65,16 @@ public class SimilarityFinderTest {
 		assertThat(result, is(equalTo(0.00)));
 	}
 	
+	@Test
+	public void testFindSimilarityNotSameValueAndLengthArrays() {
+		
+		SearchForSequenceMocker mocker = new SearchForSequenceMocker();
+		SimilarityFinder finder = new SimilarityFinder(mocker);
+		int[] seq1 = {4, 6, 8, 10};
+		int[] seq2 = {1, 2, 3};
+		double result = finder.calculateJackardSimilarity(seq1, seq2);
+		assertThat(result, is(equalTo(0.00)));
+	}
+	
 
 }
