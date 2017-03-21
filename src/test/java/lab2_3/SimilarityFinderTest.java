54e463ec81e9e84cb6dd5cfc4d5729f2970bd1d6
ciupa.kamil <194928@edu.p.lodz.pl>
2017-03-21 11:12:56 +0100

    Dodano test testCalculateJackardSimilarityOneSequenceIsNull w klasie testowej SimilarityFinderTest

--- src/test/java/lab2_3/SimilarityFinderTest.java
+++ src/test/java/lab2_3/SimilarityFinderTest.java
@@ -6,10 +6,24 @@ import static org.hamcrest.CoreMatchers.*;
 import org.junit.Assert;
 import org.junit.Test;
 
+import edu.iis.mto.similarity.MockSequenceSearcher;
+import edu.iis.mto.similarity.SimilarityFinder;
+
 
 
 public class SimilarityFinderTest {
 
+	SimilarityFinder finder = new SimilarityFinder(new MockSequenceSearcher());
 	
+	@Test
+	public void testCalculateJackardSimilarityOneSequenceIsNull() {
+		
+		int[] seq1 = {};
+		int[] seq2 = {2, 4, 5, 7, 8};
+		
+		double result = finder.calculateJackardSimilarity(seq1, seq2);
+		
+		Assert.assertThat(result, is(0d));
+	}
 	
 }
