c039ad0d3e55bf0b0c4155465893ff22c8a77639
Aleksander Kaczmarczyk <axelkko@gmail.com>
2017-03-21 10:42:08 +0100

    Utworzenie dublera: MockSequenceSearcher

---
+++ src/test/java/edu/iis/mto/similarity/MockSequenceSearcher.java
@@ -0,0 +1,13 @@
+package edu.iis.mto.similarity;
+
+import edu.iis.mto.search.SearchResult;
+import edu.iis.mto.search.SequenceSearcher;
+
+public class MockSequenceSearcher implements SequenceSearcher {
+
+	public SearchResult search(int key, int[] seq) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
