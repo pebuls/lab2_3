3f45a826753e06f0e0d181f0a9436aedf425859f
KhaledWielki <adam.rezner95@gmail.com>
2017-03-25 17:20:23 +0100

    Important hamcrest core matchers added.

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderUnitTests.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderUnitTests.java
@@ -2,6 +2,8 @@ package edu.iis.mto.similarity;
 
 
 import static org.junit.Assert.*;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.hamcrest.CoreMatchers.*;
 
 import org.junit.Test;
 
