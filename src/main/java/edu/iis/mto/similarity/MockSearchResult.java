386f070adbc58c9f3707e8b6ed4d3ee6655630d4
KhaledWielki <adam.rezner95@gmail.com>
2017-03-25 17:39:10 +0100

    MockSearchResult class added

---
+++ src/main/java/edu/iis/mto/similarity/MockSearchResult.java
@@ -0,0 +1,18 @@
+package edu.iis.mto.similarity;
+
+import edu.iis.mto.search.SearchResult;
+
+public class MockSearchResult implements SearchResult{
+
+	private boolean found;
+    private int position;
+    
+	public boolean isFound() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	public int getPosition() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
