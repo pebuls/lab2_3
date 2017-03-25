ea1e2cfa6d3735f2119cc11b975fd9d1e33e8470
KhaledWielki <adam.rezner95@gmail.com>
2017-03-25 17:18:31 +0100

    SimilarityFinderUnitTest class added.

---
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderUnitTests.java
@@ -0,0 +1,14 @@
+package edu.iis.mto.similarity;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimilarityFinderUnitTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
