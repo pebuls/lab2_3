4759531fba9c00cb41b6a91c0f0ce685491c9f69
Patron <patroneq@gmail.com>
2017-03-26 15:38:33 +0200

    dodanie testu dla 2 roznych tablic

--- src/test/java/edu/iis/mto/similarity/test/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/test/SimilarityFinderTest.java
@@ -1,33 +1,27 @@
 package edu.iis.mto.similarity.test;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
+import edu.iis.mto.search.SequenceSearcher;
+import edu.iis.mto.similarity.SequenceSearcherImpl;
 import edu.iis.mto.similarity.SimilarityFinder;
 
 public class SimilarityFinderTest {
-	
-	//SequenceMockFinder sMockFinder = new SequenceMockFinder();
-	//SimilarityFinder sFinder = new SimilarityFinder(sMockFinder);
 
-	@Test
-	public void testCalculateJackardSimilarity() {
-		int[] seq1 = {};
-		int[] seq2 = {};
-		Double result = SimilarityFinder.calculateJackardSimilarity(seq1, seq2);
-		assertThat(1.0, is(equalTo(result)));
-	}
-	@Ignore
-	@Test
-	 	public void testSame() throws NullPointerException {
-	 		int[] seq1 = {4,7,3};
-			int[] seq2 = {7,3,4};
-	 		double result = SimilarityFinder.calculateJackardSimilarity(seq1, seq2);
-			assertThat(result, is(equalTo(1.0)));
-	 	}
-
+	SequenceSearcher sequenceSearcher = new SequenceSearcherImpl();
+ 	SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSearcher);
+ 
+ 	@Test
+ 	public void differentArraysTest() {
+ 
+ 		int[] seq1 = {1,2,3};
+ 		int[] seq2 = {10,20,30};
+ 
+ 		double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
+ 
+ 		assertThat(result, is(equalTo(0.0)));
+ 	}
 }
