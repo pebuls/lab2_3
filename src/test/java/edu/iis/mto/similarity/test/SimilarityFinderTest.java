88a8919e10bd61471fd9a35c0bea1320b0fccce9
Patron <patroneq@gmail.com>
2017-03-26 15:43:00 +0200

    dodanie statycznego wywolania klasy SimilarityFinder

--- src/test/java/edu/iis/mto/similarity/test/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/test/SimilarityFinderTest.java
@@ -20,7 +20,7 @@ public class SimilarityFinderTest {
  		int[] seq1 = {1,2,3};
  		int[] seq2 = {10,20,30};
  
- 		double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
+ 		double result = SimilarityFinder.calculateJackardSimilarity(seq1, seq2);
  
  		assertThat(result, is(equalTo(0.0)));
  	}
@@ -31,7 +31,7 @@ public class SimilarityFinderTest {
  		int[] seq1 = {1,2,3};
  		int[] seq2 = {1,2,3};
  
- 		double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
+ 		double result = SimilarityFinder.calculateJackardSimilarity(seq1, seq2);
  
  		assertThat(result, is(equalTo(1.0)));
  	}
@@ -42,7 +42,7 @@ public class SimilarityFinderTest {
  		int[] seq1 = {};
  		int[] seq2 = {};
  
- 		double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
+ 		double result = SimilarityFinder.calculateJackardSimilarity(seq1, seq2);
  
  		assertThat(result, is(equalTo(1.0)));
  	}
@@ -53,7 +53,7 @@ public class SimilarityFinderTest {
  		int[] seq1 = {1,2,3};
  		int[] seq2 = {3,4,5};
  
- 		double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
+ 		double result = SimilarityFinder.calculateJackardSimilarity(seq1, seq2);
  
  		assertThat(result, is(equalTo(0.2)));
  	}
