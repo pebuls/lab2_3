25be4f8e87b0a3079ed27e0554e7ee99adcfde9d
Iryna Shkliarenko <irina.shkliarenko@gmail.com>
2017-03-27 23:30:20 +0200

    SimilarityFinderTest setUp added

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,14 +1,21 @@
 package edu.iis.mto.similarity;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 import org.junit.Test;
 
 public class SimilarityFinderTest {
+	
+	private SequenceSearcherMocked sequenceSearcher;
+	private SimilarityFinder similarityFinder;
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void setUp() {
+		sequenceSearcher = new SequenceSearcherMocked();
+		similarityFinder = new SimilarityFinder(sequenceSearcher);
 	}
+	
+	
 
 }
