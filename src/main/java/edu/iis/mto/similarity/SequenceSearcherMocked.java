a21921325c201cb6a5ffa444fed012f3cbf59424
Iryna Shkliarenko <irina.shkliarenko@gmail.com>
2017-03-27 23:10:01 +0200

    SequenceSearcherMockedMocked search method implementation 1

--- src/main/java/edu/iis/mto/similarity/SequenceSearcherMocked.java
+++ src/main/java/edu/iis/mto/similarity/SequenceSearcherMocked.java
@@ -1,11 +1,24 @@
 package edu.iis.mto.similarity;
 
+import java.util.ArrayList;
+
 import edu.iis.mto.search.SearchResult;
 import edu.iis.mto.search.SequenceSearcher;
 
 public class SequenceSearcherMocked implements SequenceSearcher{
 
+	
 	public SearchResult search(int key, int[] seq) {
+
+		
+		for(int i=0; i<seq.length; i++) {
+			if (seq[i] == key) {
+				return new SearchResultMocked(i, true);
+			}
+			
+		}
+		
+		
 		
 		return null;
 	}
