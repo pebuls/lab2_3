fd456e3bab9bef79523d9d5ca18b895ccf08deed
Iryna Shkliarenko <irina.shkliarenko@gmail.com>
2017-03-27 22:41:59 +0200

    SearchResultMocked constructor added

--- src/main/java/edu/iis/mto/similarity/SearchResultMocked.java
+++ src/main/java/edu/iis/mto/similarity/SearchResultMocked.java
@@ -3,8 +3,15 @@ package edu.iis.mto.similarity;
 import edu.iis.mto.search.SearchResult;
 
 public class SearchResultMocked implements SearchResult{
+	
 	int position;
 	boolean isFound;
+	
+	public SearchResultMocked(int position, boolean isFound) {
+		super();
+		this.position = position;
+		this.isFound = isFound;
+	}
 
 	public boolean isFound() {
 		return isFound;
