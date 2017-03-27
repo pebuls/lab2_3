3f23e9b92eec531ee4aa19c5e963f41da9bf89ee
Iryna Shkliarenko <irina.shkliarenko@gmail.com>
2017-03-27 23:37:39 +0200

    SimilarityFinderTest emtySequencesTest

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,19 +1,30 @@
 package edu.iis.mto.similarity;
 
-import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.Before;
 import static org.hamcrest.CoreMatchers.*;
 
-import org.junit.Test;
 
 public class SimilarityFinderTest {
 	
 	private SequenceSearcherMocked sequenceSearcher;
 	private SimilarityFinder similarityFinder;
 
-	@Test
+	@Before
 	public void setUp() {
 		sequenceSearcher = new SequenceSearcherMocked();
 		similarityFinder = new SimilarityFinder(sequenceSearcher);
+	}
+	
+	@Test
+	public void emptySequences() {
+		int[] seq1 = {};
+		int[] seq2 = {};
+		
+		Assert.assertThat(similarityFinder.calculateJackardSimilarity(seq1, seq2), is(equalTo(1.0)));
+		
+		
 	}
 	
 	
