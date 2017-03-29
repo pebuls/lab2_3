ebb0ef0f0d243a33d6fe1de61126d5935e73e89e
195868 Kwiecien Gaspar <195868@edu.p.lodz.pl>
2017-03-29 21:18:56 +0200

    Implementacja metody search w klasie MockSearchResult

--- src/main/java/edu/iis/mto/search/MockSequenceSearcher.java
+++ src/main/java/edu/iis/mto/search/MockSequenceSearcher.java
@@ -1,7 +1,13 @@
 package edu.iis.mto.search;
 
 public class MockSequenceSearcher implements SequenceSearcher{
-	public SearchResult search(int key, int[] sequence) {
+	public SearchResult search(int key, int[] seq) {
+		
+		for (int at = 0; at < seq.length; at++) {
+			if (seq[at] == key) {
+				return new MockSearchResult(true, at);
+			}
+		}
 		return new MockSearchResult();
 	}
 }
