2b0571f104200b22ec8c59637354efcf440e553a
195868 Kwiecien Gaspar <195868@edu.p.lodz.pl>
2017-03-29 23:27:43 +0200

    Klasa MockSequenceSearcher przechowuje dane o parametrach i ilosci wywolan metody search

--- src/main/java/edu/iis/mto/search/MockSequenceSearcher.java
+++ src/main/java/edu/iis/mto/search/MockSequenceSearcher.java
@@ -1,7 +1,16 @@
 package edu.iis.mto.search;
 
 public class MockSequenceSearcher implements SequenceSearcher{
+	
+	int key;
+	int[] sequence;
+	int callCounter = 0;
+	
 	public SearchResult search(int key, int[] seq) {
+		
+		this.key = key;
+		this.sequence = seq.clone();
+		callCounter++;
 		
 		for (int at = 0; at < seq.length; at++) {
 			if (seq[at] == key) {
