d36bb5f397c409c36fa52c4b0c5936232927ad69
195868 Kwiecien Gaspar <195868@edu.p.lodz.pl>
2017-03-29 20:49:26 +0200

    Dodanie składowych prywatnych refaktoryzacja metod w klasie MockSearchResult

--- src/main/java/edu/iis/mto/search/MockSearchResult.java
+++ src/main/java/edu/iis/mto/search/MockSearchResult.java
@@ -2,11 +2,16 @@ package edu.iis.mto.search;
 
 public class MockSearchResult implements SearchResult{
 	
+	public final int NOT_FOUND = -1;
+	
+	private boolean found;
+	private int position;
+	
 	public boolean isFound() {
-		return false;
+		return found;
 	}
 	
 	public int getPosition() {
-		return -1;
+		return position;
 	}
 }
