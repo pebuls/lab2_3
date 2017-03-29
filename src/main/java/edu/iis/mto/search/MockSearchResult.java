bc71374b875f00f0f52ff9e4c4665cdd9f496385
195868 Kwiecien Gaspar <195868@edu.p.lodz.pl>
2017-03-29 21:02:04 +0200

    Dodanie konstruktorow dla klasy MockSearchResult

--- src/main/java/edu/iis/mto/search/MockSearchResult.java
+++ src/main/java/edu/iis/mto/search/MockSearchResult.java
@@ -4,8 +4,17 @@ public class MockSearchResult implements SearchResult{
 	
 	public final int NOT_FOUND = -1;
 	
-	private boolean found;
-	private int position;
+	private boolean found = false;
+	private int position = NOT_FOUND;
+	
+	public MockSearchResult() {
+		
+	}
+	
+	public MockSearchResult(boolean found, int position) {
+		this.found = found;
+		this.position = position;
+	}
 	
 	public boolean isFound() {
 		return found;
