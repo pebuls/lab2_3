cc88872bd132e8115af08851e10c10b7ba63c3e1
195868 Kwiecien Gaspar <195868@edu.p.lodz.pl>
2017-03-29 22:21:18 +0200

    Test dla dwoch pustych tablic

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,11 +1,24 @@
 package edu.iis.mto.similarity;
 
 import static org.junit.Assert.*;
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
 import edu.iis.mto.search.*;
 
 public class SimilarityFinderTest {
 	MockSearchResult mockSearchResult = new MockSearchResult();
 	MockSequenceSearcher mockSequenceSearcher = new MockSequenceSearcher();
+	SimilarityFinder similarityFinder = new SimilarityFinder(mockSequenceSearcher);
 	
+	@Test
+	public void twoEmptyArrays() {
+		int[] seq1 = new int[0];
+		int[] seq2 = new int[0];
+		
+		double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
+		double expectedResult = 1.0;
+		
+		assertThat(result, is(equalTo(expectedResult)));
+	}
 	
 }
