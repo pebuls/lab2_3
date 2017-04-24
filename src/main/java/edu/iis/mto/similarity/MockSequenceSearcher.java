a9540891e564f689b6e3eef559ce95e3644fdd60
Aleksandra Dura <Aleksandra.Dura@sdqac.rd.tp.pl>
2017-04-24 23:49:49 +0200

    refactored MockSequenceSearcher

--- src/main/java/edu/iis/mto/similarity/MockSequenceSearcher.java
+++ src/main/java/edu/iis/mto/similarity/MockSequenceSearcher.java
@@ -5,8 +5,10 @@ import edu.iis.mto.search.SearchResult;
 import edu.iis.mto.search.SequenceSearcher;
 
 public class MockSequenceSearcher implements SequenceSearcher{
+    int count;
     public SearchResult search(int i, int[] ints) {
         int pos = 0;
+        count++;
         for (int j : ints) {
             if (i == j) {
                 return new MockSearchResult(true,pos);
