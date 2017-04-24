4a09db6bf8d3ca549e7cfa691e5854c8b85ddadd
Aleksandra Dura <Aleksandra.Dura@sdqac.rd.tp.pl>
2017-04-24 23:39:04 +0200

    created MockSequenceSearcher

---
+++ src/main/java/edu/iis/mto/similarity/MockSequenceSearcher.java
@@ -0,0 +1,18 @@
+package edu.iis.mto.similarity;
+
+
+import edu.iis.mto.search.SearchResult;
+import edu.iis.mto.search.SequenceSearcher;
+
+public class MockSequenceSearcher implements SequenceSearcher{
+    public SearchResult search(int i, int[] ints) {
+        int pos = 0;
+        for (int j : ints) {
+            if (i == j) {
+                return new MockSearchResult(true,pos);
+            }
+            pos++;
+        }
+        return new MockSearchResult(false, -1);
+    }
+}
