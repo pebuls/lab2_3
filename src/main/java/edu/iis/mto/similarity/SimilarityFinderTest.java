0685a02ccec26dc5b3f307792750afa21f872450
Khandree <a.lis.marciniak@gmail.com>
2017-04-25 02:04:45 +0200

    Created jackardSimilarityWithEmptyArrays test

--- src/main/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/main/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,6 +1,11 @@
 package edu.iis.mto.similarity;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.emptyIterable;
 
 public class SimilarityFinderTest {
 
@@ -11,5 +16,14 @@ public class SimilarityFinderTest {
     public void setUp() throws Exception {
         mockSequenceSearcher = new MockSequenceSearcher();
         finder = new SimilarityFinder(mockSequenceSearcher);
+    }
+
+    @Test
+    public void jackardSimilarityWithEmptyArrays() throws Exception {
+
+        double actual = finder.calculateJackardSimilarity(new int[0], new int[0]);
+
+        assertThat(actual, is(1.d));
+        assertThat(mockSequenceSearcher.calls, emptyIterable());
     }
 }
