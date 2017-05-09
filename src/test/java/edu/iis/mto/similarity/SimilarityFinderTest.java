3d86d7e5eb9941f8ba29c6e119635bd003a17ade
Przyjemski.Maciej <180264@edu.p.lodz.pl>
2017-05-09 13:54:56 +0200

    dodanie testu sprawdzającego wynik dla równej długości sekwencji z innymi wartościami

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -55,4 +55,12 @@ public class SimilarityFinderTest {
 		assertThat(result, is(expected));
 	}
 
+	@Test
+	public void calculateJackardSimilarityForNotEqualSequences() throws Exception {
+		int[] seq1 = { 1, 3 };
+		int[] seq2 = { 2, 1 };
+		final double expected = 1.0 / 3.0d;
+		double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
+		assertThat(result, is(expected));
+	}
 }
